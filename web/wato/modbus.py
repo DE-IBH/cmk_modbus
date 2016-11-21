84ab3d4e0256c49fdf0e9c94dd2455870a1deff2
Mathias Krüger <noreply@ibh.de>
2016-11-21 21:51:05 +0100

    Initial commit.

---
+++ web/wato/modbus.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8; py-indent-offset: 4 -*-
+
+# cmk_modbus - Check_MK plugin to poll data from modbus-enabled modbus_devices
+#
+# Authors:
+#   Mathias Krüger <noreply@ibh.de>
+#
+# Copyright Holder:
+#   2016 (C) IBH IT-Service GmbH [http://www.ibh.de/]
+#
+# License:
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this package; if not, write to the Free Software
+#   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+#
+
+register_rule(group,
+    "special_agents:modbus",
+    Tuple(
+        title = _("Check Modbus devices"),
+        help = _( "Configure address, tcp-port and type of device according to the supported devices. "
+                  "New devices can be added, by dropping a json file into the special_agent/modbus_devices folder."),
+        elements = [
+           Integer( title = _("Port"), default_value=502),
+           TextAscii( title = _("Device-Type")),
+        ]
+    ),
+    match = "first")
+
+
